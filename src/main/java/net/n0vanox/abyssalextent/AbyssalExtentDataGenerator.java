69a3f9e7bc85070726fa79283e1239e11583492c
Saumel Tomko <sjt44051@uga.edu>
2026-09-23 19:02:52 -0400

    Initial commit

---
+++ src/main/java/net/n0vanox/abyssalextent/AbyssalExtentDataGenerator.java
@@ -0,0 +1,11 @@
+package net.n0vanox.abyssalextent;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AbyssalExtentDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
